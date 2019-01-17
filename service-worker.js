758c63b9915b32d837b5c8145f03a1090083ad63
JAV27 <joshviktorov@Joshs-Air-2.fios-router.home>
2019-01-17 18:41:32 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Student-Toolkit/precache-manifest.be4bac5b65e3a85ccce23070446e2b36.js"
+  "/Student-Toolkit/precache-manifest.587c63e1df02cfe120df5d98f6510fb2.js"
 );
 
 workbox.clientsClaim();
